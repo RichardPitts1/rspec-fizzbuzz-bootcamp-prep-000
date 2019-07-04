fa0198548e930294a576f0aedda4d6b4a91207b6
Richard Pitts <richardepitts@gmail.com>
2019-07-04 11:55:47 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz.rb
+++ spec/fizzbuzz.rb
@@ -2,9 +2,9 @@
 # See README.md for instructions on how to do this
 
 
-def fizzbuzz(int)
-  if int % 3 == 0 
+def fizzbuzz(i)
+  if i % 3 == 0 
     "Fizz"
-  elsif int % 5 == 0
+  elsif i % 5 == 0
     "Buzz:"
 end
