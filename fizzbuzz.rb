37c84fb127be2d5b0aee50c753e93db07b010450
Richard Pitts <richardepitts@gmail.com>
2019-07-04 12:09:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 
 
 
-# def fizzbuzz(int)
-#   if int % 3 == 0 
-#     "Fizz"
-#   elsif int % 5 == 0
-#     "Buzz:"
-# end
+def fizzbuzz(int)
+  if int % 3 == 0 
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz:"
+end
